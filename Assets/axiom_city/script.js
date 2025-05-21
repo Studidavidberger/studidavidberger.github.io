bb4c9c6e8bf73adfba75de271068cb08b9c0d4f5
davidos_studi <david.ber0169@gmail.com>
2025-05-21 08:42:12 +0200

    Ajout fonctionnalité pour mal-voyants.

--- Assets/axiom_city/script.js
+++ Assets/axiom_city/script.js
@@ -1,18 +1,27 @@
+// Fichier script.js HUB DAVID BERGER - AXCIT
+
+const contrast_range = document.getElementById('contrast_range');
+const contrast_value = document.getElementById('contrast_value');
+const color_range = document.getElementById('color_range');
+const color_value = document.getElementById('color_value');
+const other_range = document.getElementById('other_range');
+const other_value = document.getElementById('other_value');
+const burger = document.getElementById("burger_links");
+const menu_access = document.getElementById('menu_access');
+const site = document.getElementById('site');
+
+// Menu burger.
 function ft_burger() {
-    
-    const burger = document.getElementById("burger_links");
-    
     if (burger.style.display === "flex") {
         burger.style.display = "none";
-    
     } else {
         burger.style.display = "flex";
         burger.style.flexDirection = "column";
-}
+    }
 }
 
+// Fonction de génération de liens parmi la liste dans la base de donnée.
 function ft_randomlink () {
-
     let links= [
         'http://localhost/1',
         'http://localhost/2',
@@ -20,14 +29,53 @@ function ft_randomlink () {
         'http://localhost/4',
         'http://localhost/5'
         ];
-    
     let random = Math.floor(Math.random() * links.length);
-
     let url = links[random];
-
-    console.log("pick", url, "type", typeof(url));
-    console.log("nb of links", links.length);
-
+    console.log(url, "on", links.length);
     window.location.href = url;
 }
 
+// Affichage temps reel des valeurs des boutons.
+document.addEventListener('DOMContentLoaded', () => {
+    contrast_value.textContent = contrast_range.value;
+    color_value.textContent = color_range.value;
+    other_value.textContent = other_range.value;
+
+    contrast_range.addEventListener('input', () => {
+        let currentContrast = contrast_range.value;
+        contrast_value.textContent = currentContrast;
+    });
+    color_range.addEventListener('input', () => {
+        let currentColor = color_range.value;
+        color_value.textContent = currentColor;
+    });
+    other_range.addEventListener('input', () => {
+        let currentOther = other_range.value;
+        other_value.textContent = currentOther;
+    });
+});
+
+function ft_accessibility () {
+    if (menu_access.style.display === "block")
+    {
+        menu_access.style.display = "none";
+    } else {
+        menu_access.style.display = "block";
+    }
+}
+
+function ft_protanopie() {
+    site.style.filter = 'hue-rotate(45deg)';
+}
+function ft_deuteranopie () {
+    site.style.filter = 'hue-rotate(90deg)';
+}
+function ft_tritanopie () {
+    site.style.filter = 'hue-rotate(180deg)';
+}
+function ft_achromatopsie () {
+    site.style.filter = 'grayscale(1)';
+}
+function ft_normal_color () {
+    site.style.filter = 'hue-rotate(0deg)';
+}
--- bashrc.sh
+++ bashrc.sh
@@ -4,6 +4,6 @@ echo ~/.bashrc > ~/.bashrc.bak
 echo "alias clone_hub='git clone https://github.com/Studidavidberger/studidavidberger.github.io hub'" >> ~/.bashrc
 source ~/.bashrc
 alias
-
+echo "Use the alias command tu clone github Repo : clone_hub"
 exit 0
 
